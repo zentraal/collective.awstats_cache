ceaab8467532432189abea9330348e2032b126df
Luca Pisani <luca.pisani@abstract.it>
2016-11-21 12:16:15 +0100

    Update viewlets.py

--- src/collective/awstats_cache/browser/viewlets.py
+++ src/collective/awstats_cache/browser/viewlets.py
@@ -10,20 +10,29 @@ class AwstatsStatisticsViewlet(ViewletBase):
     def get_statistics(self):
         """ get statistics for context url
         """
-        portal_state = getMultiAdapter(
-            (self.context, self.request), name=u'plone_portal_state'
-        )
-        site = portal_state.portal()
-        url = self.context.absolute_url().replace(site.absolute_url(), '')
-        quoted_url = urllib.quote(url)
-
-        results = Session.execute(
-            'SELECT * FROM statistics WHERE url="%s"' % quoted_url).fetchone()
-        if results:
-            return dict(zip(COLUMNS, results))
-        return {
+        statistics = {
             'entry': 0,
             'bandwidth': 0,
             'exit': 0,
             'pages': 0
         }
+        portal_state = getMultiAdapter(
+            (self.context, self.request), name=u'plone_portal_state'
+        )
+        site = portal_state.portal()
+        url = self.context.absolute_url().replace(site.absolute_url(), '')
+        if url == '':
+           url = '/'
+        quoted_url = urllib.quote(url)
+
+        results = Session.execute(
+            'SELECT * FROM statistics WHERE url="%s"' % quoted_url).fetchone()
+        if results:
+            statistics = dict(zip(COLUMNS, results))
+        if statistics and self.context.portal_type == "File":
+            results_dw = Session.execute(
+                'SELECT * FROM statistics WHERE url="%s"' % quoted_url).fetchone()
+            if results_dw:
+                downloads = dict(zip(COLUMNS, results_dw))
+                statistics['downloads'] = downloads['pages']
+        return statistics
